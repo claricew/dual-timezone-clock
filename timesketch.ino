c218ca46d64eec99b676126e151e589d097dbb4b
Clarice Wong <claricebwong30@yahoo.com>
2015-08-26 12:04:17 -0700

    Update timesketch.ino

--- timesketch.ino
+++ timesketch.ino
@@ -15,50 +15,59 @@ LiquidCrystal_I2C lcd(I2C_ADDR,En_pin,Rw_pin,Rs_pin,D4_pin,D5_pin,D6_pin,D7_pin)
 //pin locations for I2C
 byte bcdToDec(byte val)
 {
-return ( (val/16*10) + (val%16) );
+  return ( (val/16*10) + (val%16) );
 }
+
 void getDateDs1307(byte *second,byte *minute,byte *hour)
 {
-Wire.beginTransmission(DS1307_I2C_ADDRESS);
-Wire.write(0);
-Wire.endTransmission();
-Wire.requestFrom(DS1307_I2C_ADDRESS, 7);
-*second = bcdToDec(Wire.read() & 0x7f);
-*minute = bcdToDec(Wire.read());
-*hour = bcdToDec(Wire.read() & 0x3f);
+  Wire.beginTransmission(DS1307_I2C_ADDRESS);
+  Wire.write(0);
+  Wire.endTransmission();
+  Wire.requestFrom(DS1307_I2C_ADDRESS, 7);
+  
+  *second = bcdToDec(Wire.read() & 0x7f);
+  *minute = bcdToDec(Wire.read());
+  *hour = bcdToDec(Wire.read() & 0x3f);
 }
+
 void setup()
 {
-byte second, minute, hour;
-Wire.begin();
-lcd.begin(20, 4);
+  byte second, minute, hour;
+  Wire.begin();
+  lcd.begin(20, 4);
 }
+
 void loop()
 {
-byte second, minute, hour, seoulhour;
-String s, m, h, sh;
-getDateDs1307(&second, &minute, &hour); //read in user input
-// ensure two digit spacing
-if (second < 10) { s = "0" + String(second); } else { s = String(second); }
-if (minute < 10) { m = "0" + String(minute); } else { m = String(minute); }
-if (hour < 10) { h = " " + String(hour); } else { h = String(hour); }
-// time conversion
-if (hour + 16 < 24) {
-seoulhour = hour + 16;
-if (seoulhour < 10) { sh = " " + String(seoulhour) }
-else { sh = String(seoulhour) }
-}
-else {
-seoulhour = (hour + 16) - 24;
-if (seoulhour < 10) { sh = " " + String(seoulhour) }
-else { sh = String(seoulhour) }
-}
-lcd.setBacklightPin(BACKLIGHT_PIN,POSITIVE);
-lcd.setBacklight(HIGH);
-lcd.home (); // go home
-lcd.print("Seoul: " + sh + ":" + m + ":" + s + " ");
-lcd.setCursor ( 0, 1 ); // go to the 2nd line
-lcd.print(" SF: " + h + ":" + m + ":" + s + " ");
-lcd.setCursor ( 0, 2 ); // go to the third line
-delay(1000); //wait 1 second
+  byte second, minute, hour, seoulhour;
+  String s, m, h, sh;
+  getDateDs1307(&second, &minute, &hour); //read in user input
+  // ensure two digit spacing
+
+  if (second < 10) { s = "0" + String(second); } else { s = String(second); }
+  if (minute < 10) { m = "0" + String(minute); } else { m = String(minute); }
+  if (hour < 10) { h = " " + String(hour); } else { h = String(hour); }
+  // time conversion
+  
+  if (hour + 16 < 24) {
+    seoulhour = hour + 16;
+    if (seoulhour < 10) { sh = " " + String(seoulhour) }
+    else { sh = String(seoulhour) }
+  }
+  else {
+    seoulhour = (hour + 16) - 24;
+    if (seoulhour < 10) { sh = " " + String(seoulhour) }
+    else { sh = String(seoulhour) }
+  }
+  
+  lcd.setBacklightPin(BACKLIGHT_PIN,POSITIVE);
+  lcd.setBacklight(HIGH);
+  lcd.home (); // go home
+  
+  lcd.print("Seoul: " + sh + ":" + m + ":" + s + " ");
+  lcd.setCursor ( 0, 1 ); // go to the 2nd line
+  lcd.print(" SF: " + h + ":" + m + ":" + s + " ");
+  lcd.setCursor ( 0, 2 ); // go to the third line
+  
+  delay(1000); //wait 1 second
 }
